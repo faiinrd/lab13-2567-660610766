796c858b67af27970f28518af17bb5467e91fb08
faiinrd <nopradakrit@gmail.com>
2024-09-02 17:34:26 +0700

    finished

--- src/app/search/[searchInput]/page.js
+++ src/app/search/[searchInput]/page.js
@@ -1,11 +1,11 @@
 "use client";
 
-import { MovieRow } from "@/components/MovieRow";
-import { movieDB } from "@/libs/movieDB";
+import MovieRow from "@components/MovieRow";
+import { movieDB } from "@lib/movieDB";
 
-export default function SearchResultPage() {
+export default function SearchResultPage({params}) {
   //tip1 : before filtering movie, replace all "%20" with " " (space) in the input
-  // const processedSearchInput = ...
+  const processedSearchInput = params.searchInput.replaceAll("%20", " ");
 
   /*
   tip2 : Use "includes" string method to check substring
@@ -17,14 +17,30 @@ export default function SearchResultPage() {
     you code here...
   );
   */
+  const filteredMovies = movieDB.filter((movie) => {
+    const lowtitle = movie.title.toLocaleLowerCase();
+    const lowinput = processedSearchInput.toLocaleLowerCase();
+    return lowtitle.includes(lowinput);
+ });
+ console.log(filteredMovies);
 
   return (
     <div>
       <p className="fw-bold fs-4 text-center my-0">
-        Searching &quot; ... &quot;
+        Searching &quot; {processedSearchInput} &quot;
       </p>
-      <p className="fw-bold fs-4 text-center">Found ... result(s)</p>
+      <p className="fw-bold fs-4 text-center">Found {filteredMovies.length} result(s)</p>
       {/* Use  "filteredMovies" variable to map-loop rendering MovieRow component */}
+      {filteredMovies.map((movie,i) => (
+        <MovieRow 
+        key={movie.id}
+        id={movie.id}
+        title={movie.title}
+        detail={movie.detail}
+        rating={movie.rating}
+        number={i + 1}
+        />
+      ))}
     </div>
   );
 }
